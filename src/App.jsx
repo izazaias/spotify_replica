cd79cf83b010f82a2970b8360fbe8aafc61dcd9d
Isaías dos Santos <izazaiasdossantos.is@gmail.com>
2025-02-11 17:55:06 -0300

    Subindo projeto para o repositorio

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header"
+import Main from "./components/Main"
+
+const App = () => {
+  return <>
+    <Header />
+    <Main />  
+  </>
+}
+
+export default App
---
+++ src/App2.jsx
@@ -0,0 +1,37 @@
+const App2 = () => {
+  return (
+    <div>App2</div>
+  )
+}
+
+export default App2
+
+// rafce
+
+// Arrow function
+// const App2 = () => <h1>Olá, Mundo!</h1>;
+
+// Nomeação de Componentes
+// PascalCase
+
+// Nomeação de Variavel, Função...
+// camelCaseConcundaDoCamelo
+
+// export default, posso exportar com qualquer nome e sem chaves
+// export 'sem default', só posso importar entre chaves e com o nome exportado
+
+// Self closing tag
+// <Header></Header>
+// <Header />
+
+// Nomeação de classes em CSS
+// Metodologia BEM
+// Blocks
+// Elements
+// Modifiers
+// bloco__elemento--modificador
+// header
+// header__link
+// header__link--small
+
+// Tag vazia em React se chama Fragment ou Fragmento
